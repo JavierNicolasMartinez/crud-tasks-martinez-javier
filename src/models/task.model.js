3f157309fe0d936aaffd1b65c9eb4fec6aec123f
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-08-07 10:19:19 -0300

    Modelos listos con las especificaciones completas y funcionamiento correcto de los links

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,1 +1,18 @@
 import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Task = sequelize.define("task", {
+  title: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  isComplete: {
+    type: DataTypes.BOOLEAN,
+    default: false,
+  },
+});
